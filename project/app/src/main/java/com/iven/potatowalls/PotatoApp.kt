abc85ccfde65627266c864842bf28a46d5813687
enricocid <enrio2588@gmail.com>
2019-07-24 16:06:20 +0200

    Remove unused variable
    Signed-off-by: enricocid <enrio2588@gmail.com>

--- project/app/src/main/java/com/iven/potatowalls/PotatoApp.kt
+++ project/app/src/main/java/com/iven/potatowalls/PotatoApp.kt
@@ -6,21 +6,15 @@ val mPotatoPreferences: PotatoPreferences by lazy {
     PotatoApp.prefs
 }
 
-val mSystemAccent: Int by lazy {
-    PotatoApp.systemAccent
-}
-
 class PotatoApp : Application() {
     companion object {
         lateinit var prefs: PotatoPreferences
-        var systemAccent = 0
         var backgroundColor = 0
         var potatoColor = 0
     }
 
     override fun onCreate() {
         prefs = PotatoPreferences(applicationContext)
-        systemAccent = Utils.getSystemAccentColor(applicationContext)
         backgroundColor = prefs.backgroundColor
         potatoColor = prefs.potatoColor
         super.onCreate()
