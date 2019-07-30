daf2a6eb060a5775a98580507aa371fb49b95206
enricocid <enrico2588@gmail.com>
2019-07-30 22:41:25 +0200

    Clean code
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- project/app/src/main/java/com/iven/potatowalls/PotatoApp.kt
+++ project/app/src/main/java/com/iven/potatowalls/PotatoApp.kt
@@ -9,14 +9,10 @@ val mPotatoPreferences: PotatoPreferences by lazy {
 class PotatoApp : Application() {
     companion object {
         lateinit var prefs: PotatoPreferences
-        var backgroundColor = 0
-        var potatoColor = 0
     }
 
     override fun onCreate() {
         prefs = PotatoPreferences(applicationContext)
-        backgroundColor = prefs.backgroundColor
-        potatoColor = prefs.potatoColor
         super.onCreate()
     }
 }
